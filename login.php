4d4959723f8f336a4358b930f4d5deffb3368856
Luis Miguel Molina <luismiguel.molina@campusfp.es>
2022-01-13 11:11:37 +0100

    login2

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<?php
+if (isset($_POST["iniciar"])) {
+    $user = $_POST["mail"];
+    $pass = $_POST["password"];
+    if ($user == "admin@admin.com" && $pass == "123456") {
+        $mensaje = "Usuario y contraseña correcta";
+        $estado = true;
+    } else {
+        $mensaje = "Usuario y contraseña incorrecta";
+        $estado = false;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -60,21 +74,27 @@
         <div class="card login-form">
             <div class="card-body">
                 <h3 class="card-title text-center">Log in to Codepen</h3>
+                <?php
+                if (isset($_POST["iniciar"])) {
+                ?>
+                    <p style="text-align: center;<?= $estado ? "color:green;" : "color:red;" ?>"><?= $mensaje ?></p>
+                <?php
+                } ?>
                 <div class="card-text">
                     <!--
 			<div class="alert alert-danger alert-dismissible fade show" role="alert">Incorrect username or password.</div> -->
-                    <form>
+                    <form action="" method="POST">
                         <!-- to error: add class "has-danger" -->
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="email" name="mail" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
                             <a href="#" style="float:right;font-size:12px;">Forgot password?</a>
-                            <input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
+                            <input type="password" name="password" class="form-control form-control-sm" id="exampleInputPassword1">
                         </div>
-                        <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                        <button type="submit" class="btn btn-primary btn-block" name="iniciar">Sign in</button>
 
                         <div class="sign-up">
                             Don't have an account? <a href="#">Create One</a>
